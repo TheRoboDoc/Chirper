33a896b80c18a1173d2d726384f68daa8c7d8ee6
RoboDoc <18618265+TheRoboDoc@users.noreply.github.com>
2023-12-28 18:49:22 +0200

    Created a json converter to settings and viceversa

---
+++ Data/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Chirper.Data
+{
+    public static class Converter
+    {
+        public static T? JsonToData<T>(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public static string? DataToJson(object data)
+        {
+            Type fromType = data.GetType();
+
+            return JsonConvert.SerializeObject(value: data, type: fromType, null);
+        }
+    }
+}
