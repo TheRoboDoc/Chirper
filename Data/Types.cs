e6377510ae77e5498ccb7ccfa80b445ca7d72ce0
RoboDoc <18618265+TheRoboDoc@users.noreply.github.com>
2023-12-28 18:48:45 +0200

    Added setting types

---
+++ Data/Types.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Data
+{
+    public static class Types
+    {
+        public struct ChatToggle
+        {
+            public long ChannelID { get; set; }
+
+            public bool Enabled { get; set; }
+        }
+    }
+}
