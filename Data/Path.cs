93c8b68b94ec285947b5e35ada35b614b277da36
RoboDoc <18618265+TheRoboDoc@users.noreply.github.com>
2023-12-28 18:48:57 +0200

    Added setting paths

---
+++ Data/Path.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Data
+{
+    public static class Path
+    {
+        private static readonly string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        private static readonly string settingsPath = $@"{basePath}/Settings";
+
+        public static readonly string togglePath = $@"{settingsPath}/Toggle";
+    }
+}
